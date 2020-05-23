aaa4485576e6335e4618cb527893d4fd118ca923
soungupta <soungupta@ebay.com>
2020-05-23 14:23:03 -0700

    updated the downloaded filenames

--- parse_crawl.py
+++ parse_crawl.py
@@ -3,22 +3,14 @@
 import sys, re, os, requests, mimetypes
 import pandas as pd
 
-def i2alpha(x):
-    if not x:
-        raise AssertionError()
-    s = ''
-    while x != 0:
-        s = chr(97 + x%26 - 1) + s
-        x /= 26
-    return s
-
 if len(sys.argv) != 2 :
     raise ValueError("Enter ./parse_crawl <csv_file>")
 
 df = pd.read_csv(filepath_or_buffer = sys.argv[1])
 for i,row in df.iterrows():
-    path = "results/" + row["Name"] + "/"
-    print(row["Name"]),
+    path = "results/" + str(row["Name"]) + "/"
+    file_tag = path + str(row["Name"]) + " - "
+    print(row["Name"])
     j = 0
     for x in row:
         val = str(x)
@@ -30,10 +22,10 @@ for i,row in df.iterrows():
             raise AssertionError()
         if not os.path.exists(path):
             os.makedirs(path)
-        col_name = i2alpha(j)
-        print(col_name),
+        col_name = str(df.columns[j-1])
+        file_name = file_tag + col_name
+        print("\t- " + file_name)
         response = requests.get(urls[0], allow_redirects=True)
         content_type = response.headers['content-type']
         extension = mimetypes.guess_extension(content_type)
-        open(path + col_name + extension, 'wb').write(response.content)
-    print("")
+        open(file_name + extension, 'wb').write(response.content)
