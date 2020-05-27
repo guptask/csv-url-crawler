b12679eca1edf9fea8226056888f7d2191d007bb
soungupta <soungupta@ebay.com>
2020-05-27 11:18:54 -0700

    refined the script to allow download even if http header is missing; added comments as well

--- parse_crawl.py
+++ parse_crawl.py
@@ -26,9 +26,10 @@ for i,row in df.iterrows():
 
     for x in row:
         val = str(x)
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', val)
         j += 1
 
+        # Parse for URLs
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', val)
         if not urls:
             continue
         if len(urls) > 1:
@@ -36,26 +37,32 @@ for i,row in df.iterrows():
         if not os.path.exists(path):
             os.makedirs(path)
 
+        # Form the filename
         col_name = str(df.columns[j-1])
         file_name = file_tag + col_name
-        found = glob.glob(file_name + "*")
 
+        # Search for existing files with matching name
+        found = glob.glob(file_name + "*")
         if len(found) > 1:
             raise AssertionError()
         if len(found) == 1:
             print("\t[old] " + col_name)
             continue
 
+        # Extract file extension from HTTP header
+        extension = ""
         response = requests.get(urls[0], allow_redirects=True)
         if r'Content-Type' not in response.headers.keys():
             print("\t[err] " + col_name)
-            log = open('errlog.txt','a')
-            log.write(unique_id + r' - ' + col_name + "\n")
-            log.close()
-            continue
+            log_fd = open('errlog.txt','a')
+            log_fd.write(unique_id + r' - ' + col_name + "\n")
+            log_fd.close()
+        else:
+            print("\t[new] " + col_name)
+            content_type = response.headers['Content-Type']
+            extension = mimetypes.guess_extension(content_type)
 
-        print("\t[new] " + col_name)
-        content_type = response.headers['Content-Type']
-        extension = mimetypes.guess_extension(content_type)
-        open(file_name + extension, 'wb').write(response.content)
+        file_fd = open(file_name + extension, 'wb')
+        file_fd.write(response.content)
+        file_fd.close()
 
